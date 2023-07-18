f13240654473a542f140fcae36365b7a1c08c886
Emmanuel Then <emmanuel.then@outlook.com>
2023-07-17 22:19:33 -0400

    updated password missmatch rendering, and minor design changes

--- app/account/update-password/page.tsx
+++ app/account/update-password/page.tsx
@@ -3,66 +3,88 @@ import React, { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import Link from 'next/link'
+import ShinyText from '@/app/components/ShinyText'
 
 type Props = {
-    
+
 }
 
-const page = ({  }: Props) => {
+const page = ({ }: Props) => {
     const [password, setPassword] = useState('');
+    const [reEnterPassword, setReEnterPassword] = useState('')
     const [loading, setLoading] = useState(true);
     const supabase = createClientComponentClient();
+    const router = useRouter();
 
     const handleUpdatePassword = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         await supabase.auth.updateUser({ password: `${password}` })
-
+        router.push('/dashboard');
+        router.refresh();
     }
 
     return (
-        <form
-            className='flex-1 flex flex-col w-full px-8 max-w-lg justify-center gap-2'
-            onSubmit={handleUpdatePassword}
-        >
-            <label className="text-md" htmlFor="password">
-                Enter new password
-            </label>
-            <input
-                className="rounded-md px-4 py-2 bg-slate4 border "
-                type="password"
-                name="password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-                pattern='/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=]).{8,}$/'
-                required
-                placeholder="••••••••"
-            />
-            <div id='password-requirements' className='leading-snug text-[8px] md:text-[10px] text-blue9/60 ml-2 mb-6'>
-                <p>At least 8 characters</p>
-                <p>Mix of letters and numbers</p>
-                <p>At least 1 special character</p>
-                <p>At least 1 lowercase letter and 1 uppercase letter</p>
+        <div className='flex justify-center items-center h-screen w-full'>
+            <div className='flex-1 flex flex-col items-center w-full px-8 max-w-lg justify-center gap-2' >
+                <div className='text-center mb-6'>
+                    <ShinyText text='Reset password'/>
+                </div>
+                <form
+                    className=" px-20 py-12 rounded-lg shadow-blackA9 shadow-[0_8px_15px] flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+                    onSubmit={handleUpdatePassword}
+                >
+                    <label className="text-md" htmlFor="password">
+                        Enter new password
+                    </label>
+                    <input
+                        className="rounded-md px-4 py-2 bg-slate4 border "
+                        type="password"
+                        name="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        pattern='/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=]).{8,}$/'
+                        required
+                        placeholder="••••••••"
+                    />
+                    <div id='password-requirements' className='leading-snug text-[8px] md:text-[10px] text-blue9/60 ml-2 mb-6'>
+                        <p>At least 8 characters</p>
+                        <p>Mix of letters and numbers</p>
+                        <p>At least 1 special character</p>
+                        <p>At least 1 lowercase letter and 1 uppercase letter</p>
+                    </div>
+                    <label className="text-md" htmlFor="password">
+                        Re-enter new password
+                    </label>
+                    <input
+                        className="rounded-md px-4 py-2 bg-slate4 border mb-6"
+                        type="password"
+                        name="password"
+                        onChange={(e) => setReEnterPassword(e.target.value)}
+                        value={reEnterPassword}
+                        pattern='/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=]).{8,}$/'
+                        required
+                        placeholder="••••••••"
+                    />
+                    <button
+                        className="bg-blue9 hover:opacity-80 rounded px-4 py-2 text-white hover:cursor-pointer shadow-blackA9 shadow-[0_4px_7px]"
+                        disabled={!password || !reEnterPassword}
+                    >
+                        {loading ? 'Update password' : <p className='scale-in-center'> Loading...</p>}
+                    </button>
+                    <div>
+                        {password === reEnterPassword ? (
+                            ''
+                        ) : (
+                            <div className='flex justify-center'>
+                                <span className='text-red9'>
+                                    Passwords dont match
+                                </span>
+                            </div>
+                        )}
+                    </div>
+                </form>
             </div>
-            <label className="text-md" htmlFor="password">
-                Re-enter new password
-            </label>
-            <input
-                className="rounded-md px-4 py-2 bg-slate4 border mb-6"
-                type="password"
-                name="password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-                pattern='/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=]).{8,}$/'
-                required
-                placeholder="••••••••"
-            />
-            <button
-                className="bg-blue9 hover:opacity-80 rounded px-4 py-2 text-white hover:cursor-pointer shadow-blackA9 shadow-[0_4px_7px]"
-                disabled={!password}
-            >
-                {loading ? 'Update password' : <p className='scale-in-center'> Loading...</p>}
-            </button>
-        </form>
+        </div>
     )
 }
 
--- app/components/auth-form.tsx
+++ app/components/auth-form.tsx
@@ -11,12 +11,14 @@ import TwitterButton from './TwitterButton'
 import ForgotPasswordModal from '../signin/components/ForgotPasswordModal'
 import ConfirmationButton from './ConfirmationButton'
 import DialogButton from './DialogButton'
+import ShinyText from './ShinyText'
 
 
 export default function Login() {
     const [loading, setLoading] = useState(true)
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [reEnterPassword, setReEnterPassword] = useState('')
     const [view, setView] = useState('sign-in')
     const router = useRouter()
     const supabase = createClientComponentClient()
@@ -89,159 +91,171 @@ export default function Login() {
                     up
                 </p>
             ) : (
-                <form
-
-                    className=" px-20 py-12 rounded-lg shadow-blackA9 shadow-[0_8px_15px] flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-                    onSubmit={view === 'sign-in' ? handleSignIn : handleSignUp}
-                >
-                    <label className="text-md" htmlFor="email">
-                        Email
-                    </label>
-                    <input
-                        className="rounded-md px-4 py-2 bg-slate4 border mb-6"
-                        name="email"
-                        onChange={(e) => setEmail(e.target.value)}
-                        value={email}
-                        placeholder="you@example.com"
-                        required
-                    />
-                    <label className="text-md" htmlFor="password">
-                        Password
-                    </label>
-                    {view === 'sign-up' ?
-                        (
-                            <>
-                                <input
-                                    className="rounded-md px-4 py-2 bg-slate4 border "
-                                    type="password"
-                                    name="password"
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    value={password}
-                                    pattern='/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=]).{8,}$/'
-                                    required
-                                    placeholder="••••••••"
-                                />
-                                <div id='password-requirements' className='leading-snug text-[8px] md:text-[10px] text-blue9/60 ml-2 mb-6'>
-                                    <p>At least 8 characters</p>
-                                    <p>Mix of letters and numbers</p>
-                                    <p>At least 1 special character</p>
-                                    <p>At least 1 lowercase letter and 1 uppercase letter</p>
-                                </div>
-                                <label className="text-md" htmlFor="password">
-                                    Re-enter Password
-                                </label>
+                <div className='flex flex-col items-center justify-center mb-6'>
+                    <div>
+                        <ShinyText
+                            text={view === 'sign-in' ? 'Sign in' : view === 'sign-up' ? 'Create account' : ''}
+                        />
+                    </div>
+                    <form
+                        className=" px-20 py-12 rounded-lg shadow-blackA9 shadow-[0_8px_15px] flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+                        onSubmit={view === 'sign-in' ? handleSignIn : handleSignUp}
+                    >
+                        <label className="text-md" htmlFor="email">
+                            Email
+                        </label>
+                        <input
+                            className="rounded-md px-4 py-2 bg-slate4 border mb-6"
+                            name="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                            placeholder="you@example.com"
+                            required
+                        />
+                        <label className="text-md" htmlFor="password">
+                            Password
+                        </label>
+                        {view === 'sign-up' ?
+                            (
+                                <>
+                                    <input
+                                        className="rounded-md px-4 py-2 bg-slate4 border "
+                                        type="password"
+                                        name="password"
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        value={password}
+                                        pattern='/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=]).{8,}$/'
+                                        required
+                                        placeholder="••••••••"
+                                    />
+                                    <div id='password-requirements' className='leading-snug text-[8px] md:text-[10px] text-blue9/60 ml-2 mb-6'>
+                                        <p>At least 8 characters</p>
+                                        <p>Mix of letters and numbers</p>
+                                        <p>At least 1 special character</p>
+                                        <p>At least 1 lowercase letter and 1 uppercase letter</p>
+                                    </div>
+                                    <label className="text-md" htmlFor="password">
+                                        Re-enter Password
+                                    </label>
+                                    <input
+                                        className="rounded-md px-4 py-2 bg-slate4 border mb-6"
+                                        type="password"
+                                        name="password"
+                                        onChange={(e) => setReEnterPassword(e.target.value)}
+                                        value={reEnterPassword}
+                                        pattern='/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=]).{8,}$/'
+                                        required
+                                        placeholder="••••••••"
+                                    />
+                                </>
+                            ) :
+                            (
                                 <input
                                     className="rounded-md px-4 py-2 bg-slate4 border mb-6"
                                     type="password"
                                     name="password"
                                     onChange={(e) => setPassword(e.target.value)}
                                     value={password}
-                                    pattern='/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=]).{8,}$/'
-                                    required
                                     placeholder="••••••••"
+                                    required
                                 />
-                            </>
-
-                        ) :
-                        (
-                            <input
-                                className="rounded-md px-4 py-2 bg-slate4 border mb-6"
-                                type="password"
-                                name="password"
-                                onChange={(e) => setPassword(e.target.value)}
-                                value={password}
-                                placeholder="••••••••"
-                                required
-                            />
-
-                        )}
-                    {view === 'sign-in' && (
-                        <>
-                            <button
-                                className="bg-blue9 hover:opacity-80 rounded px-4 py-2 text-white hover:cursor-pointer"
-                                disabled={!email || !password}
-                            >
-                                {loading ? 'Sign In' : <p className='scale-in-center'> Loading...</p>}
-                            </button>
-                            <div id='signup-terms' className=' flex justify-center mb-6'>
-                                <div className='text-[10px] text-center '>
-                                    <div>
-                                        <DialogButton
-                                            buttonText='Forgot password?'
-                                            dialogTitle=''
-                                            dialogDesc=''
-                                            content={
-                                                <ForgotPasswordModal
-                                                    modalContentDisplay=''
-                                                    confirmationDisplay=''
-                                                />
-                                            }
-                                            saveButton=''
-                                            buttonDisplay='hidden'
-                                            myOwnButtonDisplay='hidden'
-                                            myOwnButton={
-                                                <ConfirmationButton
-                                                    buttonText='Continue'
-                                                    toastTitle='Email sent to:'
-                                                    toastContent='session.user.email'
-                                                />
-                                            }
-
-                                        />
+                            )}
+                        {view === 'sign-in' && (
+                            <>
+                                <button
+                                    className="bg-blue9 hover:opacity-80 rounded px-4 py-2 text-white hover:cursor-pointer"
+                                    disabled={!email || !password}
+                                >
+                                    {loading ? 'Sign In' : <p className='scale-in-center'> Loading...</p>}
+                                </button>
+                                <div id='signup-terms' className=' flex justify-center mb-6'>
+                                    <div className='text-[10px] text-center '>
+                                        <div>
+                                            <DialogButton
+                                                buttonText='Forgot password?'
+                                                dialogTitle=''
+                                                dialogDesc=''
+                                                content={
+                                                    <ForgotPasswordModal
+                                                        modalContentDisplay=''
+                                                        confirmationDisplay=''
+                                                    />
+                                                }
+                                                saveButton=''
+                                                buttonDisplay='hidden'
+                                                myOwnButtonDisplay='hidden'
+                                                myOwnButton={
+                                                    <ConfirmationButton
+                                                        buttonText='Continue'
+                                                        toastTitle='Email sent to:'
+                                                        toastContent='session.user.email'
+                                                    />
+                                                }
+                                            />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-
-                            <p className="text-sm text-center">
-                                Don't have an account?
+                                <p className="text-sm text-center">
+                                    Don't have an account?
+                                    <button
+                                        className="ml-1 hover:underline text-blue9 hover:opacity-80"
+                                        onClick={() => setView('sign-up')}
+                                    >
+                                        Sign up now
+                                    </button>
+                                </p>
+                                <div className='flex justify-center w-[300px] mt-10'>
+                                    <div className='flex items-center justify-center gap-8 w-full'>
+                                        <hr className='block w-full h-[1px] border-0 bg-[#e8e8e8]' />
+                                        <span className='text-xs  text-slate8'>Or</span>
+                                        <hr className='block w-full h-[1px] border-0 bg-[#e8e8e8]' />
+                                    </div>
+                                </div>
+                                <div className='flex flex-col gap-2' id='providers-buttons'>
+                                    <div>
+                                        <GoogleButton />
+                                    </div>
+                                    <div>
+                                        <DiscordButton />
+                                    </div>
+                                    <div>
+                                        <TwitterButton />
+                                    </div>
+                                </div>
+                            </>
+                        )}
+                        {view === 'sign-up' && (
+                            <>
                                 <button
-                                    className="ml-1 hover:underline text-blue9 hover:opacity-80"
-                                    onClick={() => setView('sign-up')}
-
+                                    className={`${password === reEnterPassword ? 'mb-6' : ''} bg-blue9 hover:opacity-80 rounded px-4 py-2 text-white hover:cursor-pointer`}
+                                    disabled={!email || !password || !reEnterPassword}
                                 >
-                                    Sign up now
+                                    Sign Up
                                 </button>
-                            </p>
-                            <div className='flex justify-center w-[300px] mt-10'>
-                                <div className='flex items-center justify-center gap-8 w-full'>
-                                    <hr className='block w-full h-[1px] border-0 bg-[#e8e8e8]' />
-                                    <span className='text-xs  text-slate8'>Or</span>
-                                    <hr className='block w-full h-[1px] border-0 bg-[#e8e8e8]' />
-                                </div>
-                            </div>
-                            <div className='flex flex-col gap-2' id='providers-buttons'>
                                 <div>
-                                    <GoogleButton />
+                                    {password === reEnterPassword ? (
+                                        ''
+                                    ) : (
+                                        <div className='flex justify-center mb-6'>
+                                            <span className='text-red9'>
+                                                Passwords dont match
+                                            </span>
+                                        </div>
+                                    )}
                                 </div>
-                                <div>
-                                    <DiscordButton />
-                                </div>
-                                <div>
-                                    <TwitterButton />
-                                </div>
-                            </div>
-                        </>
-                    )}
-                    {view === 'sign-up' && (
-                        <>
-                            <button
-                                className="bg-blue9 hover:opacity-80 rounded px-4 py-2 text-white mb-6 hover:cursor-pointer"
-                                disabled={!email || !password}
-                            >
-                                Sign Up
-                            </button>
-                            <p className="text-sm text-center">
-                                Already have an account?
-                                <button
-                                    className="ml-1 hover:underline text-blue9 hover:opacity-80"
-                                    onClick={() => setView('sign-in')}
-                                >
-                                    Sign in now
-                                </button>
-                            </p>
-                        </>
-                    )}
-                </form>
+                                <p className="text-sm text-center">
+                                    Already have an account?
+                                    <button
+                                        className="ml-1 hover:underline text-blue9 hover:opacity-80"
+                                        onClick={() => setView('sign-in')}
+                                    >
+                                        Sign in now
+                                    </button>
+                                </p>
+                            </>
+                        )}
+                    </form>
+                </div>
             )}
         </div>
     )
